26ed0467f44c121cfe1fb1fac2d3ff8d616a32de
Brayan Corredor <brayanjcp@outlook.com>
2018-10-31 10:43:43 -0500

    Fix server

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var express=require('express');
 var http = require('http');
 
 var app=express();
-app.get('',(req,res)=>{
+app.get('/',(req,res)=>{
     res.send('Hola Mundo');
 });
+app.set('port',3000);
 var server=http.createServer(app);
 server.listen(3000,()=>{
     console.log('App listening');
