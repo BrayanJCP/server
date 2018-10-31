1b089c746b636c80155959d4e37c673a863a7034
Brayan Corredor <brayanjcp@outlook.com>
2018-10-31 14:31:51 -0500

    deploy app react

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var path=require('path');
 var port=process.env.PORT || 3000;
 var app=express();
 app.use(express.static('frontend-react'));
-
+app.get('/react', function(req, res) {
+    res.sendFile(path.join(__dirname, 'frontend-react/react-app/build', 'index.html'));
+  });
 var server=http.createServer(app);
 server.listen(port,()=>{
     console.log('App listening in the '+port);
---
+++ frontend-react/react-app/build/precache-manifest.f8d40d01385f21ee2ec5e56d9d60b6e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f626f9fe3b057ad12c8e",
+    "url": "/static/js/main.f626f9fe.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "f626f9fe3b057ad12c8e",
+    "url": "/static/css/main.fa8d5bbc.chunk.css"
+  },
+  {
+    "revision": "1cde32de9f5a8d62c6a58e4910e4b3d6",
+    "url": "/index.html"
+  }
+];
