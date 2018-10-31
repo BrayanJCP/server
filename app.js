2f6b7af0c22e14ee6c4b8abef348c63538f3eb25
Brayan Corredor <brayanjcp@outlook.com>
2018-10-31 09:53:39 -0500

    Ajuste servidor

--- app.js
+++ app.js
@@ -3,11 +3,8 @@ var http = require('http');
 
 var app=express();
 
-app.listen(3000,()=>{
-    console.log('App listening in the port 3000');
-});
 app.get('/',(req,res)=>{
     res.send('Hola Mundo');
 });
 var server = http.createServer(app);
-server.listen(3000);
+server.listen();
