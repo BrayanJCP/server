4a981d73b4691fcb05e69df8cd7f3e7edf1c380f
Brayan Corredor <brayanjcp@outlook.com>
2018-10-31 10:06:37 -0500

    Fix server

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var http = require('http');
 
 var app=express();
 
-app.get('/',(req,res)=>{
-    res.send('Hola Mundo');
-});
 app.listen(3000,()=>{
     console.log('App listening in port 3000');
+});
+app.get('',(req,res)=>{
+    res.send('Hola Mundo');
 });
