ae71df051f9555a948c6dd53da4a1960c2f56765
Brayan Corredor <brayanjcp@outlook.com>
2018-10-31 11:43:35 -0500

    Fix server

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var express=require('express');
 var http = require('http');
+const path = require('path')
+const PORT = process.env.PORT || 3000
 
 
-var app=express();
-app.use(express.static('frontend-react'));
-
-
-var server=http.createServer(app);
-server.listen(3000,()=>{
-    console.log('App listening');
+var app=express()
+.use(express.static(path.join(__dirname, 'frontend-react')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs');
+app.get('/other',(req,res)=>{
+    res.send("Funciona");
+});
+app.listen(PORT,()=>{
+    console.log('App listening in the port '+PORT);
 });
