b1a588b2a3d4802c048b6d91745c479538882531
Brayan Corredor <brayanjcp@outlook.com>
2018-10-31 15:07:08 -0500

    Create app angular

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ app.use(express.static(pathreact));
 app.get('/react', function(req, res) {
     res.sendFile(path.join(pathreact, 'index.html'));
   });
+/*Angular */
+var pathangular=path.join(__dirname,'frontend-angular/angular-app/dist/angular-app');
+app.use(express.static(pathangular));
+app.get('/angular', function(req, res) {
+    res.sendFile(path.join(pathangular, 'index.html'));
+  });
+/*Inicio del servidor */
 var server=http.createServer(app);
 server.listen(port,()=>{
     console.log('App listening in the '+port);
