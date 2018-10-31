1b416a80fe250b03d4cf93489a39111f792ed971
Brayan Corredor <brayanjcp@outlook.com>
2018-10-31 11:34:08 -0500

    Fix server

--- app.js
+++ app.js
@@ -3,14 +3,10 @@ var http = require('http');
 
 
 var app=express();
-app.set('port',3000);
 app.use(express.static('frontend-react'));
 
 
 var server=http.createServer(app);
 server.listen(3000,()=>{
-    console.log('App listening');
-});
-server.on('listening',()=>{
     console.log('App listening');
 });
