704f5a868fb54c1c8c8882cb06a2eb8ae2192f55
Brayan Corredor <brayanjcp@outlook.com>
2018-11-07 10:25:40 -0500

    Ajuste de routing de angular

--- frontend-angular/angular-app/src/app/app-routing.module.ts
+++ frontend-angular/angular-app/src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const appRoutes: Routes = [
     imports: [
       RouterModule.forRoot(
         appRoutes,
-        { enableTracing: true } // <-- debugging purposes only
+        { enableTracing: true, useHash:true } // <-- debugging purposes only
       )
       // other imports here
     ],
